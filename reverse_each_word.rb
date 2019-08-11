4099e91542dc6065ab7ca49c7ec9ca8483bf5b57
Brad Alexander <brad.alexander913@gmail.com>
2019-08-11 20:51:13 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,10 @@
 def reverse_each_word(string)
 sentence_Array = string.split(' ')
 sentence_Array.each do |word|
+  i = 0
   new_word = word.reverse!
-#i = new_string.count
-puts new_word
+  new_sentence[i].push(new_word)
+  i += 1
 end
 end
 
