2c6ee4d334e56e9b199e7541d9f434eacaecbb5b
Brad Alexander <brad.alexander913@gmail.com>
2019-08-11 20:49:59 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,10 @@
 def reverse_each_word(string)
 sentence_Array = string.split(' ')
+sentence_Array.each do |word|
+  new_word = word.reverse!
 #i = new_string.count
-puts sentence_Array
+puts new_word
+end
 end
 
 reverse_each_word("Hello there, and how are you?")
