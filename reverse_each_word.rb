9bbae8aa82565f986f3c2493e22fef8dc6046463
Brad Alexander <brad.alexander913@gmail.com>
2019-08-11 20:22:30 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,20 +1,4 @@
 def reverse_each_word(string)
-  strArr = []
-  newStr = []
-  strArr.push(string.split(" "))
-  strArr.each do |word|
-    puts(word)
-    end_point = word.length 
-    new_word = ''
-      while end_point > 0
-         end_point -= 1
-        new_word += word[end_point]
-       
-    #    puts(new_word)
-      end 
-      newStr << new_word
-    end 
-  puts newStr
- end
 
-reverse_each_word("Hello there, and how are you?")
+
+end
