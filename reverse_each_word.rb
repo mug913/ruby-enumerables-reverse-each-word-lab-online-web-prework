45427cbf7c81a9c4b42350c371f0c7d2b2234525
Brad Alexander <brad.alexander913@gmail.com>
2019-08-11 20:05:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   strArr = []
   strArr.push(string.split(" "))
   strArr[0].reverse!
-  puts strArr[1]
+  puts strArr[0]
  end
 
 reverse_each_word("Hello there, and how are you?")
