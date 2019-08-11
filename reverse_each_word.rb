a25ea8536767c3ef203b8eef9f7634e57fe73b53
Brad Alexander <brad.alexander913@gmail.com>
2019-08-11 19:57:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,6 +2,6 @@ def reverse_each_word(string)
   strArr = []
   revArr = []
   strArr.push(string.split(" "))
-  revArr = strArr.reverse_each
+  revArr.push(strArr.reverse_each)
   puts revArr[0]
 end
