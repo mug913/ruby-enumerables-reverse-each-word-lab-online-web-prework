fc4350d7dad90d22ebf47c4e5dacef439a4fa46c
Brad Alexander <brad.alexander913@gmail.com>
2019-08-11 20:19:22 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -6,8 +6,9 @@ def reverse_each_word(string)
     end_point = word.length 
     new_word = ''
       while end_point > 0
+         end_point -= 1
         new_word += word[end_point]
-        end_point -= 1
+       
         puts(new_word)
       end 
       newStr << new_word
