dcc88ebb2e02f9b024ef404090a08663adcdf8f5
Brad Alexander <brad.alexander913@gmail.com>
2019-08-11 20:18:41 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,12 +1,14 @@
 def reverse_each_word(string)
+  strArr = []
   newStr = []
   strArr.push(string.split(" ").reverse!)
   strArr.each do |word|
     end_point = word.length 
     new_word = ''
       while end_point > 0
-        end_point -= 1
         new_word += word[end_point]
+        end_point -= 1
+        puts(new_word)
       end 
       newStr << new_word
     end 
